3342b879679f1f3db37b1e4cbe545642a0e2bf78
vals2960 <finoanavalisoa2960@gmail.com>
2025-05-06 18:01:29 +0300

    vals

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
 
 const { sequelize } = require('./Modele/index');
 
+app.use(cors());
 app.use(express.json());
 
 const userroute = require('./route/route');
